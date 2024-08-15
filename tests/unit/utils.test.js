5ac65d91f5742d38cf3de74fd09d87af35b03d1e
Aaron Watters <awatters@flatironinstitute.org>
2024-08-15 10:53:42 -0400

    async examples

--- tests/unit/utils.test.js
+++ tests/unit/utils.test.js
@@ -1,6 +1,6 @@
 // tests/unit/utils.test.js
 import { describe, it, expect } from 'vitest';
-import { sum } from '../../lib/main';
+import { sum, async_sum } from '../../lib/main';
 
 describe('sum function', () => {
   it('adds 1 + 2 to equal 3', () => {
@@ -9,5 +9,15 @@ describe('sum function', () => {
 
   it('adds -1 + 1 to equal 0', () => {
     expect(sum(-1, 1)).toBe(0);
+  });
+});
+
+describe('async_sum function', () => {
+  it('adds 1 + 2 to equal 3', async () => {
+    expect(await async_sum(1, 2)).toBe(3);
+  });
+
+  it('adds -1 + 1 to equal 0', async () => {
+    expect(await async_sum(-1, 1)).toBe(0);
   });
 });
