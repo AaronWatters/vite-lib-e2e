9d7f7aa6afb49ae7dd1084f4822c3122bdc16676
Aaron Watters <awatters@flatironinstitute.org>
2024-08-23 14:39:05 -0400

    example of async exception

--- tests/unit/utils.test.js
+++ tests/unit/utils.test.js
@@ -1,6 +1,7 @@
 // tests/unit/utils.test.js
 import { describe, it, expect } from 'vitest';
 import { sum, async_sum } from '../../lib/main';
+import { async_error } from '../../lib/utils';
 
 describe('sum function', () => {
   it('adds 1 + 2 to equal 3', () => {
@@ -19,5 +20,12 @@ describe('async_sum function', () => {
 
   it('adds -1 + 1 to equal 0', async () => {
     expect(await async_sum(-1, 1)).toBe(0);
+  });
+});
+
+describe('asyncFunctionThatThrows', () => {
+  it('should throw an error', async () => {
+    // Test that the async function throws an error
+    await expect(async_error()).rejects.toThrow('Something went wrong');
   });
 });
